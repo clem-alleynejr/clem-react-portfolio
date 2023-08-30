d24833e213bf0a50f88454ce0689fd07444d2a6b
clem-alleynejr <clemjr@hotmail.com>
2023-08-30 15:51:39 -0600

    starting build page loader

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,68 @@
+import './index.scss'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
   return (
     <div className="container about-page">
       <div className="text-zone">
         <h1>
-          <AnimatedLetters strArray={'About Me'.split('')} idx={15} />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'About Me'.split('')}
+            idx={15}
+          />
         </h1>
-        <p>Having worked in the structural engineering industry and recently completing a rigorous full-stack software engineering intensive program, I aim to leverage my technical background to transition into software development, recognizing its vital role in society. As a problem solver at heart, I have always been drawn to math and logic-based challenges. Professionally, I’ve worn many hats including project manager, structural analyzer/designer, and tower climber/inspector.</p>
+        <p>
+          Having worked in the structural engineering industry and recently
+          completing a rigorous full-stack software engineering intensive
+          program, I aim to leverage my technical background to transition into
+          software development, recognizing its vital role in society. As a
+          problem solver at heart, I have always been drawn to math and
+          logic-based challenges. Professionally, I’ve worn many hats including
+          project manager, structural analyzer/designer, and tower
+          climber/inspector.
+        </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+        </div>
       </div>
     </div>
   )
